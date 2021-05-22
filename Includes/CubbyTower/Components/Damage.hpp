970047afa18089e8634df9c33ad2bd29dc94d815
Chris Ohk <utilforever@gmail.com>
2021-05-22 23:33:57 +0900

    feat(ecs): Create struct 'Damage'
    - This struct stores the damage of the tower

---
+++ Includes/CubbyTower/Components/Damage.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYTOWER_DAMAGE_HPP
+#define CUBBYTOWER_DAMAGE_HPP
+
+namespace CubbyTower
+{
+//!
+//! \brief Damage struct.
+//!
+//! This struct stores the damage of the tower.
+//!
+struct Damage
+{
+    int damage;
+};
+}  // namespace CubbyTower
+
+#endif  // CUBBYTOWER_DAMAGE_HPP
