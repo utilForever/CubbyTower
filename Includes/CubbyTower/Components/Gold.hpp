0023ea19343882ca7643236fb99315fa129666e4
Chris Ohk <utilforever@gmail.com>
2021-05-23 00:09:13 +0900

    feat(ecs): Create struct 'Gold'
    - This struct stores the gold of the entity

---
+++ Includes/CubbyTower/Components/Gold.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYTOWER_GOLD_HPP
+#define CUBBYTOWER_GOLD_HPP
+
+namespace CubbyTower
+{
+//!
+//! \brief Gold struct.
+//!
+//! This struct stores the gold of the entity.
+//!
+struct Gold
+{
+    int amount;
+};
+}  // namespace CubbyTower
+
+#endif  // CUBBYTOWER_GOLD_HPP
