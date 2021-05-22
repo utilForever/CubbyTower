a00a62061970e1ed16fbd19555ac0b1e44aa9209
Chris Ohk <utilforever@gmail.com>
2021-05-23 00:51:35 +0900

    feat(ecs): Add constant 'ARROW_TOWER_LV1_PRICE'
    - The price of arrow tower at level 1

---
+++ Includes/CubbyTower/Commons/Constants.hpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYTOWER_CONSTANTS_HPP
+#define CUBBYTOWER_CONSTANTS_HPP
+
+namespace CubbyTower
+{
+//! The price of arrow tower at level 1.
+constexpr static int ARROW_TOWER_LV1_PRICE = 100;
+}  // namespace CubbyTower
+
+#endif  // CUBBYTOWER_CONSTANTS_HPP
