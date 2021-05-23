c44eac5319520804b592bde02751e53aa317ffa7
Chris Ohk <utilforever@gmail.com>
2021-05-23 14:04:12 +0900

    test: Create unit test file to test function 'Withdraw()'

---
+++ Tests/UnitTests/Helpers/GoldHelpersTests.cpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include <doctest.h>
+
+#include <CubbyTower/Components/Gold.hpp>
+#include <CubbyTower/Helpers/GoldHelpers.hpp>
+
+#include <entt/entt.hpp>
+
+using namespace CubbyTower;
+
+TEST_CASE("[GoldHelpers] - Withdraw")
+{
+    entt::registry registry;
+    auto entity = registry.create();
+
+    CHECK_FALSE(Withdraw(registry, entity, 100));
+
+    registry.emplace<Gold>(entity, 100);
+    CHECK(Withdraw(registry, entity, 50));
+    CHECK_FALSE(Withdraw(registry, entity, 100));
+}
