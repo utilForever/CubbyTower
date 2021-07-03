18d8430bb02a414af520ccd5a029a6b12475b17b
MinJune Yi <matia2.yi@gmail.com>
2021-07-03 20:48:55 +0900

    add distance struct

---
+++ Includes/CubbyTower/Components/Distance.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYTOWER_DISTANCE_HPP
+#define CUBBYTOWER_DISTANCE_HPP
+
+namespace CubbyTower
+{
+//!
+//! \brief Distance struct.
+//!
+//! This struct stores the distance a monster has traveled.
+//!
+struct Distance
+{
+    int s;
+};
+}  // namespace CubbyTower
+
+#endif  // CUBBYTOWER_DISTANCE_HPP
