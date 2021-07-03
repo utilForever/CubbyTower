e1e14fd78c0e81b06e4c2d3af895888f419e2e8f
MinJune Yi <matia2.yi@gmail.com>
2021-07-03 20:43:33 +0900

    add position struct

---
+++ Includes/CubbyTower/Components/Position.hpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYTOWER_POSITION_HPP
+#define CUBBYTOWER_POSITION_HPP
+
+#include <string>
+
+namespace CubbyTower
+{
+//!
+//! \brief Position struct.
+//!
+//! This struct stores the position of the entity.
+//!
+struct Position 
+{
+    int x;
+    int y;
+};
+}  // namespace CubbyTower
+
+#endif  // CUBBYTOWER_POSITION_HPP
