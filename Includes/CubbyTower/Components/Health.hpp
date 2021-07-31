92f52c4855ef2ffeb38e05afa6f961990712e41a
MinJune Yi <matia2.yi@gmail.com>
2021-07-31 23:23:50 +0900

    feat: Add Health Component

---
+++ Includes/CubbyTower/Components/Health.hpp
@@ -0,0 +1,25 @@
+// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYTOWER_HEALTH_HPP
+#define CUBBYTOWER_HEALTH_HPP
+
+#include <string>
+
+namespace CubbyTower
+{
+//!
+//! \brief Health struct.
+//!
+//! This struct stores the health of the monster.
+//!
+struct Name
+{
+    std::string name;
+};
+}  // namespace CubbyTower
+
+#endif  // CUBBYTOWER_HEALTH_HPP
