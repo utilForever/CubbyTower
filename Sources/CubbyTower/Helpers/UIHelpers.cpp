77296456b73fd64b19cfc2891745ff3f3a28cf19
Chris Ohk <utilForever@gmail.com>
2021-09-25 18:45:25 +0900

    feat(gui): Add function 'CreateTowerButton()'

---
+++ Sources/CubbyTower/Helpers/UIHelpers.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include <CubbyTower/Helpers/UIHelpers.hpp>
+
+namespace CubbyTower::UI
+{
+void CreateTowerButton(
+    entt::registry& registry, const char* caption, const Position& position,
+    int price, std::function<void(entt::registry&, entt::entity)> onClick)
+{
+    // Do nothing
+}
+}  // namespace CubbyTower::UI
