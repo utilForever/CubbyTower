f51a6211f1da736be4525e9b4c89a9a61b09af31
Chris Ohk <utilforever@gmail.com>
2021-09-26 20:51:10 +0900

    feat(gui): Create struct 'Color'
    - This struct stores the color of the component

---
+++ Includes/CubbyTower/Components/Color.hpp
@@ -0,0 +1,26 @@
+// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYTOWER_COLOR_HPP
+#define CUBBYTOWER_COLOR_HPP
+
+namespace CubbyTower
+{
+//!
+//! \brief Color struct.
+//!
+//! This struct stores the color of the component.
+//!
+struct Color
+{
+    float r;
+    float g;
+    float b;
+    float a;
+};
+}  // namespace CubbyTower
+
+#endif  // CUBBYTOWER_COLOR_HPP
