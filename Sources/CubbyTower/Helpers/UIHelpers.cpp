0e1c4d5a524fea1b496312e40f00530d559f7ffa
Chris Ohk <utilforever@gmail.com>
2021-09-26 21:03:42 +0900

    feat(gui): Implement function 'CreateTowerButton()'

--- Sources/CubbyTower/Helpers/UIHelpers.cpp
+++ Sources/CubbyTower/Helpers/UIHelpers.cpp
@@ -4,6 +4,10 @@
 // personal capacity and are not conveying any rights to any intellectual
 // property of any third parties.
 
+#include <CubbyTower/Components/Color.hpp>
+#include <CubbyTower/Components/Name.hpp>
+#include <CubbyTower/Components/Size.hpp>
+#include <CubbyTower/Components/TextRenderer.hpp>
 #include <CubbyTower/Helpers/UIHelpers.hpp>
 
 namespace CubbyTower::UI
@@ -12,6 +16,17 @@ void CreateTowerButton(
     entt::registry& registry, const char* caption, const Position& position,
     int price, std::function<void(entt::registry&, entt::entity)> onClick)
 {
-    // Do nothing
+    auto entity = registry.create();
+    registry.emplace<Position>(entity, position);
+    registry.emplace<Size>(entity, Size{ 2.5f, 0.4f });
+    registry.emplace<Color>(entity, Color{ 0.6f, 0.7f, 0.5f, 1.0f });
+    registry.emplace<TextRenderer>(entity, caption,
+                                   Color{ 0.0f, 0.0f, 0.0f, 0.0f }, 0.5f);
+    registry.emplace<Name>(entity, std::to_string(price) + " Gold");
+    //registry.emplace<ShapeRenderer>(entity, Shape::DrawBox);
+    //registry.emplace<CashThresholdButtonToggler>(entity, price);
+    //registry.emplace<Button>(entity, Color{ 0.3f, 0.7f, 0.5f, 1.0f }, onClick);
+    //registry.emplace<Hoverable>(entity, Color{ 0.6f, 0.7f, 0.5f, 1.0f },
+    //                            Color{ 1.0f, 1.0f, 0.8f, 1.0f });
 }
 }  // namespace CubbyTower::UI
