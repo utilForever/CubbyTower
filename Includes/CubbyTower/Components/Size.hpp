2748a535475c8492700df06f2c438876f1716294
Chris Ohk <utilforever@gmail.com>
2021-09-26 20:49:40 +0900

    feat(gui): Create struct 'Size'
    - This struct stores the size of the component for mouse picking

---
+++ Includes/CubbyTower/Components/Size.hpp
@@ -0,0 +1,24 @@
+// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYTOWER_SIZE_HPP
+#define CUBBYTOWER_SIZE_HPP
+
+namespace CubbyTower
+{
+//!
+//! \brief Size struct.
+//!
+//! This struct stores the size of the component for mouse picking.
+//!
+struct Size
+{
+    float width;
+    float height;
+};
+}  // namespace CubbyTower
+
+#endif  // CUBBYTOWER_SIZE_HPP
