733a7f443f93fefa715ccb48fff831e0717a92b5
Chris Ohk <utilforever@gmail.com>
2021-10-17 17:23:39 +0900

    feat(gui): Create struct 'Hoverable'
    - This struct stores the value of normal/hover color

---
+++ Includes/CubbyTower/Components/Hoverable.hpp
@@ -0,0 +1,24 @@
+// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYTOWER_HOVERABLE_HPP
+#define CUBBYTOWER_HOVERABLE_HPP
+
+namespace CubbyTower
+{
+//!
+//! \brief Hoverable struct.
+//!
+//! This struct stores the value of normal/hover color.
+//!
+struct Hoverable
+{
+    Color normalColor;
+    Color hoverColor;
+};
+}  // namespace CubbyTower
+
+#endif  // CUBBYTOWER_HOVERABLE_HPP
