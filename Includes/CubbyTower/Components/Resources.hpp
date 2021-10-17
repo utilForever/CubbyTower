7a53f0b27e97c8d7a6b2f5889bf0b5cd4b70a0f0
Chris Ohk <utilforever@gmail.com>
2021-10-18 02:46:10 +0900

    feat(gui): Create struct 'Resources'
    - This struct stores resource handles

---
+++ Includes/CubbyTower/Components/Resources.hpp
@@ -0,0 +1,25 @@
+// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYTOWER_RESOURCES_HPP
+#define CUBBYTOWER_RESOURCES_HPP
+
+namespace CubbyTower
+{
+struct VertexPC;
+
+//!
+//! \brief Resources struct.
+//!
+//! This struct stores resource handles.
+//!
+struct Resources
+{
+    VertexPC* pcVertices;
+};
+}  // namespace CubbyTower
+
+#endif  // CUBBYTOWER_RESOURCES_HPP
