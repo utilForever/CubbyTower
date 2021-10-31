40031149aed675ed864bbe5c49610e0c63754361
Chris Ohk <utilforever@gmail.com>
2021-10-31 16:22:17 +0900

    feat(gui): Add code to calculate delta time

--- Sources/CubbyTower/main.cpp
+++ Sources/CubbyTower/main.cpp
@@ -11,10 +11,14 @@
 #include <SFML/Window/Window.hpp>
 #include <entt/entt.hpp>
 
+#include <chrono>
+
 using namespace CubbyTower;
 
 int main()
 {
+    using namespace std::chrono;
+
     // Create SFML window
     sf::Window window;
     window.create(sf::VideoMode(800, 600), "Cubby Tower",
@@ -24,9 +28,20 @@ int main()
     entt::registry registry;
     Game::Initialize(registry);
 
-    sf::Event newEvent;
+    sf::Event newEvent{};
+    auto lastTime = high_resolution_clock::now();
+
     while (window.isOpen())
     {
+        // Calculate delta time
+        auto now = high_resolution_clock::now();
+        auto diffTime = now - lastTime;
+        lastTime = now;
+
+        const auto durationCount =
+            static_cast<double>(duration_cast<microseconds>(diffTime).count());
+        auto deltaTime = static_cast<float>(durationCount / 1000000.0);
+
         while (window.pollEvent(newEvent))
         {
             // Close windows
@@ -45,6 +60,8 @@ int main()
                                              newEvent.mouseButton.button);
             }
         }
+
+        (void)deltaTime;
 
         window.display();
     }
