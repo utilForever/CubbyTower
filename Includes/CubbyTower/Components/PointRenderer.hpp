30c55b579d2c6b335b7f425cbdf8ba3c18bc7c3c
Chris Ohk <utilforever@gmail.com>
2022-01-05 19:55:31 +0900

    feat(gui): Create struct 'PointRenderer'
    - This struct stores the functor for rendering point

---
+++ Includes/CubbyTower/Components/PointRenderer.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYTOWER_POINT_RENDERER_HPP
+#define CUBBYTOWER_POINT_RENDERER_HPP
+
+namespace CubbyTower
+{
+//!
+//! \brief PointRenderer struct.
+//!
+//! This struct stores the functor for rendering point.
+//!
+struct PointRenderer
+{
+    int dummy = 0;
+};
+}  // namespace CubbyTower
+
+#endif  // CUBBYTOWER_POINT_RENDERER_HPP
