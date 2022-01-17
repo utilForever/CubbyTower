004b6fd3d71e1921ee71e716159a4513133cde38
Chris Ohk <utilforever@gmail.com>
2022-01-18 01:38:33 +0900

    feat(gui): Add function 'CreateLabel()'
    - Creates label to show text

--- Includes/CubbyTower/Helpers/UIHelpers.hpp
+++ Includes/CubbyTower/Helpers/UIHelpers.hpp
@@ -7,12 +7,24 @@
 #ifndef CUBBYTOWER_UI_HELPERS_HPP
 #define CUBBYTOWER_UI_HELPERS_HPP
 
+#include <CubbyTower/Components/Color.hpp>
 #include <CubbyTower/Components/Position.hpp>
 
 #include <entt/entt.hpp>
 
 namespace CubbyTower::UI
 {
+//! Creates label to show text.
+//! \param registry A registry that handles entities.
+//! \param text The text to describe the label.
+//! \param position The relative position of the label.
+//! \param color The color of the label.
+//! \param align The align of the label.
+//! \return The created label entity.
+entt::entity CreateLabel(entt::registry& registry, const char* text,
+                         const Position& position, const Color& color,
+                         float align);
+
 //! Creates tower button to buy or upgrade.
 //! \param registry A registry that handles entities.
 //! \param caption The caption to describe the button.
