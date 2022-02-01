427322ac48e9cf995684beb065f65c46dc9cee06
Chris Ohk <utilforever@gmail.com>
2022-02-01 19:33:19 +0900

    feat(gui): Create struct 'UIContext'
    - This struct stores UI states

---
+++ Includes/CubbyTower/Components/UIContext.hpp
@@ -0,0 +1,28 @@
+﻿// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYTOWER_UI_CONTEXT_HPP
+#define CUBBYTOWER_UI_CONTEXT_HPP
+
+#include <entt/entt.hpp>
+
+namespace CubbyTower
+{
+//!
+//! \brief UIContext struct.
+//!
+//! This struct stores UI states.
+//!
+struct UIContext
+{
+    // Entity for current mouse hover
+    entt::entity hover;
+    // Entity for when last mouse down
+    entt::entity down;
+};
+}  // namespace CubbyTower
+
+#endif  // CUBBYTOWER_UI_CONTEXT_HPP
