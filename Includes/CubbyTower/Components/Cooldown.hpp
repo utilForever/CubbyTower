200699a659936c74bfba0c515c659734a56fcff4
IHIHHIHI <yiminjune@snu.ac.kr>
2022-02-15 23:47:59 +0900

    Auto stash before revert of "feat: add cooldown component"
    feat: add cooldown component

---
+++ Includes/CubbyTower/Components/Cooldown.hpp
@@ -0,0 +1,25 @@
+// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYTOWER_COOLDOWN_HPP
+#define CUBBYTOWER_COOLDOWN_HPP
+
+namespace CubbyTower
+{
+//!
+//! \brief Cooldown Struct.
+//!
+//! This struct stores the data for cooldowns for attack cooldowns or ability cooldowns
+//! This is used by the tower, or abilities(might be added later)
+//!
+struct Cooldown
+{
+    float maxTime;
+    float nowTime;
+};
+}  // namespace CubbyTower
+
+#endif  // CUBBYTOWER_COOLDOWN_HPP
