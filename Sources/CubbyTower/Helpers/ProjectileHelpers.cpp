035fbed77a7e91f031f824a6d734289ab037a793
IHIHHIHI <yiminjune@snu.ac.kr>
2022-02-19 22:28:36 +0900

    feat: add make projectile

--- Sources/CubbyTower/Helpers/ProjectileHelpers.cpp
+++ Sources/CubbyTower/Helpers/ProjectileHelpers.cpp
@@ -6,14 +6,42 @@
 
 #include <CubbyTower/Components/Size.hpp>
 #include <CubbyTower/Commons/Tags.hpp>
+#include <CubbyTower/Components/Position.hpp>
 #include <CubbyTower/Helpers/ProjectileHelpers.hpp>
-
+#include <CubbyTower/Components/TargetMask.hpp>
+#include <CubbyTower/Components/Damage.hpp>
+#include <CubbyTower/Components/Velocity.hpp>
+#include <cmath>
 namespace CubbyTower
 {
-void CreateProjectile(entt::registry& registry, entt::entity tower, entt:entity enemy)
+void CreateProjectile(entt::registry& registry, entt::entity tower, entt::entity enemy)
 {
 
+
     float speed = 1.0; //TODO : add projectile speed component in tower
+
+    auto entity = registry.create();
+    registry.emplace<Tag::Projectile>(entity);
+    registry.emplace<TargetMask>(entity, registry.get<TargetMask>(tower));
+    registry.emplace<Damage>(entity, registry.get<Damage>(tower));
+    // TODO : add lifetime by attackrange / speed
+    registry.emplace<Size>(entity, 1.0f, 1.0f); // TODO : add projectile size in tower
+
+    const auto towerPosition = registry.get<Position>(tower);
+    const auto enemyPosition = registry.get<Position>(enemy);
+
+    registry.emplace<Position>(entity, towerPosition);
+
+    Position positionDiff = {enemyPosition.x - towerPosition.x, enemyPosition.y - towerPosition.y};
+
+    float distance = sqrt(positionDiff.x * positionDiff.x + positionDiff.y * positionDiff.y);
+
+    if (distance == 0){
+        registry.emplace<Velocity>(entity, 0.0f, 0.0f);
+        return;
+    }
+
+    registry.emplace<Velocity>(entity, speed * positionDiff.x / distance , speed * positionDiff.y / distance );
 
 
 }
