5f615f42be2be5a3c7d6c641e58dea433376eb65
Chris Ohk <utilforever@gmail.com>
2022-03-14 23:37:34 +0900

    feat(targeting): Create struct 'TargetMask'
    - An enumerator for identifying target's type

---
+++ Includes/CubbyTower/Commons/Enums.hpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYTOWER_ENUMS_HPP
+#define CUBBYTOWER_ENUMS_HPP
+
+namespace CubbyTower
+{
+//! \brief An enumerator for identifying target's type.
+enum TargetMask : uint8_t
+{
+    GROUND = 1 << 0,
+    AIR = 1 << 1,
+};
+}  // namespace CubbyTower
+
+#endif  // CUBBYTOWER_ENUMS_HPP
