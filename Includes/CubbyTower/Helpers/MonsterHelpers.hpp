86c41176d3157d46223ade8b8a4aaa7b62629d1b
Mingyu Lee <minkyu.lee65@gmail.com>
2022-03-19 22:18:19 +0900

    fix minor typo

--- Includes/CubbyTower/Helpers/MonsterHelpers.hpp
+++ Includes/CubbyTower/Helpers/MonsterHelpers.hpp
@@ -11,7 +11,7 @@
 
 namespace CubbyTower
 {
-//! Create a monster.
+//! Creates a monster.
 //! \param registry A registry that handles entities.
 //! \param health The health of a monster that will be created.
 //! \param typeMask A mask that contains the type of a monster.
@@ -19,7 +19,7 @@ void CreateMonster(
     entt::registry& registry, int health, int typeMask,
     std::function<void(entt::registry&, entt::entity)> OnDestroy);
 
-//! Destroy a monster.
+//! Destroys a monster.
 //! \param registry A registry that handles entities.
 //! \param entity The monster that will be destroyed.
 void DestroyMonster(entt::registry& registry, entt::entity entity);
