aeb98d9248c431b57ae581bc522154e8e86b7b68
IHIHHIHI <yiminjune@snu.ac.kr>
2022-03-19 15:45:41 +0900

    feat: add collisionsystem

---
+++ Sources/CubbyTower/Systems/CollisionSystem.cpp
@@ -0,0 +1,20 @@
+// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include <CubbyTower/Commons/Tags.hpp>
+#include <CubbyTower/Components/Inputs.hpp>
+#include <CubbyTower/Components/UIContext.hpp>
+#include <CubbyTower/Components/Upgradable.hpp>
+#include <CubbyTower/Helpers/GoldHelpers.hpp>
+#include <CubbyTower/Systems/UpgradeSystem.hpp>
+
+namespace CubbyTower
+{
+void UpdateCollisionSystem(entt::registry& registry)
+{
+    // do something
+}
+}  // namespace CubbyTower
