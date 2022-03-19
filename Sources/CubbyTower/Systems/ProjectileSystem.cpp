6a9190b7e3ef6d4727e916dc36665dcb943d14e7
IHIHHIHI <yiminjune@snu.ac.kr>
2022-03-19 21:43:39 +0900

    feat: implement projectilesystem

--- Sources/CubbyTower/Systems/ProjectileSystem.cpp
+++ Sources/CubbyTower/Systems/ProjectileSystem.cpp
@@ -5,12 +5,19 @@
 // property of any third parties.
 
 #include <CubbyTower/Commons/Tags.hpp>
+#include <CubbyTower/Components/Position.hpp>
+#include <CubbyTower/Components/Velocity.hpp>
 #include <CubbyTower/Systems/ProjectileSystem.hpp>
 
 namespace CubbyTower
 {
 void UpdateProjectileSystem(entt::registry& registry, float deltaTime)
 {
-    // do something
+    registry.view<Tag::Projectile, Position, Velocity>().each(
+        [&](auto entity, auto& pos, auto& vel) {
+            // TODO : remove projectile when out of bound
+            pos.x += vel.x * deltaTime;
+            pos.y += vel.y * deltaTime;
+        });
 }
 }  // namespace CubbyTower
