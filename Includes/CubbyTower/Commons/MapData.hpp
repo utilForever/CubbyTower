3650b4fd7a74feccc18cb5d9f1c99f60e64526c5
Chris Ohk <utilforever@gmail.com>
2022-04-08 21:18:49 +0900

    feat(monster): Add waypoint data for air monsters

--- Includes/CubbyTower/Commons/MapData.hpp
+++ Includes/CubbyTower/Commons/MapData.hpp
@@ -28,6 +28,10 @@ constexpr static Position GROUND_WAYPOINTS[] = {
     { 12.5f, 4.5f },  { 12.5f, 5.5f },  { 12.5f, 6.5f },  { 13.5f, 6.5f },
     { 14.5f, 6.5f },  { 15.5f, 6.5f },  { 15.5f, 6.5f }
 };
+
+constexpr static int AIR_WAYPOINT_COUNT = 2;
+
+constexpr static Position AIR_WAYPOINTS[] = { { 0.5f, 4.5f }, { 15.5f, 6.5f } };
 }  // namespace CubbyTower
 
 #endif  // CUBBYTOWER_MAP_DATA_HPP
