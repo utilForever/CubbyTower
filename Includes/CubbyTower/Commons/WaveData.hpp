ff2bf6222db9320fdab5a084be037ed45ae7c29d
Chris Ohk <utilforever@gmail.com>
2022-04-28 23:20:48 +0900

    feat(monster): Add wave data for wave 2

--- Includes/CubbyTower/Commons/WaveData.hpp
+++ Includes/CubbyTower/Commons/WaveData.hpp
@@ -35,6 +35,32 @@ const static WaveFactory WAVE_1_FACTORIES[] = { Monster::CreateNormalBalloon,
                                                 Monster::CreateNormalBalloon,
                                                 Monster::CreateNormalBalloon,
                                                 nullptr };
+
+constexpr static float WAVE_2_TIMES[] = { 0.00f,  1.50f,  2.50f,  3.50f,
+                                          5.00f,  6.50f,  7.50f,  8.50f,
+                                          10.00f, 10.50f, 12.00f, 13.00f,
+                                          14.00f, 15.00f, 15.50f, 17.00f,
+                                          18.00f, 19.00f, 19.00f };
+
+const static WaveFactory WAVE_2_FACTORIES[] = { Monster::CreateNormalBalloon,
+                                                Monster::CreateNormalBalloon,
+                                                Monster::CreateNormalBalloon,
+                                                Monster::CreateNormalBalloon,
+                                                Monster::CreateNormalBalloon,
+                                                Monster::CreateNormalBalloon,
+                                                Monster::CreateNormalBalloon,
+                                                Monster::CreateNormalBalloon,
+                                                Monster::CreateNormalBalloon,
+                                                Monster::CreateNormalBalloon,
+                                                Monster::CreateNormalBalloon,
+                                                Monster::CreateNormalBalloon,
+                                                Monster::CreateNormalBalloon,
+                                                Monster::CreateNormalBalloon,
+                                                Monster::CreateNormalBalloon,
+                                                Monster::CreateNormalBalloon,
+                                                Monster::CreateNormalBalloon,
+                                                Monster::CreateNormalBalloon,
+                                                nullptr };
 }  // namespace CubbyTower
 
 #endif  // CUBBYTOWER_WAVE_DATA_HPP
