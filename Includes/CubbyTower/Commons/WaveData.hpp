390322a5d1cce495b958cef42b1d23a516c9f420
Chris Ohk <utilforever@gmail.com>
2022-04-28 23:06:57 +0900

    feat(monster): Add constant for wave count

---
+++ Includes/CubbyTower/Commons/WaveData.hpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYTOWER_WAVE_DATA_HPP
+#define CUBBYTOWER_WAVE_DATA_HPP
+
+namespace CubbyTower
+{
+//! The number of waves.
+constexpr static int WAVE_COUNT = 2;
+}  // namespace CubbyTower
+
+#endif  // CUBBYTOWER_WAVE_DATA_HPP
