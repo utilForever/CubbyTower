f10229cc1c3ffb22a997bd9daabbc35424070196
Chris Ohk <utilforever@gmail.com>
2022-04-29 22:00:48 +0900

    feat(monster): Create struct 'Spawner'
    - This struct spawns a monster

---
+++ Includes/CubbyTower/Components/Spawner.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYTOWER_SPAWNER_HPP
+#define CUBBYTOWER_SPAWNER_HPP
+
+namespace CubbyTower
+{
+//!
+//! \brief Spawner struct.
+//!
+//! This struct spawns a monster.
+//!
+struct Spawner
+{
+    float waveTime = 0.0f;
+};
+}  // namespace CubbyTower
+
+#endif  // CUBBYTOWER_SPAWNER_HPP
