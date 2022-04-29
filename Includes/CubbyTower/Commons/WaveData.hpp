8d4425fe9f32934cfa7f3343fb8c0340a374bd6b
Chris Ohk <utilforever@gmail.com>
2022-04-29 22:01:03 +0900

    feat(monster): Add constant 'WAVE_START_TIME'
    - The start time of the wave

--- Includes/CubbyTower/Commons/WaveData.hpp
+++ Includes/CubbyTower/Commons/WaveData.hpp
@@ -19,6 +19,8 @@ using WaveFactory = std::function<void(entt::registry&, const Position&)>;
 constexpr static int WAVE_COUNT = 2;
 //! The start position of the wave.
 constexpr static Position WAVE_START_POSITION = { 0.5f, 4.5f };
+//! The start time of the wave.
+constexpr static float WAVE_START_TIME = -0.1f;
 
 constexpr static float WAVE_1_TIMES[] = { 0.00f,  2.00f,  4.00f,  6.00f,
                                           8.00f,  10.00f, 12.00f, 14.00f,
