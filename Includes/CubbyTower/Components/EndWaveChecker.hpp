3e6f30e77abc081995eeddcc53aed0eaec826fca
Chris Ohk <utilforever@gmail.com>
2022-04-29 21:46:40 +0900

    feat(monster): Create struct 'EndWaveChecker'
    - This struct checks if the wave is completed, and take actions on it

---
+++ Includes/CubbyTower/Components/EndWaveChecker.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYTOWER_END_WAVE_CHECKER_HPP
+#define CUBBYTOWER_END_WAVE_CHECKER_HPP
+
+namespace CubbyTower
+{
+//!
+//! \brief EndWaveChecker struct.
+//!
+//! This struct checks if the wave is completed, and take actions on it.
+//!
+struct EndWaveChecker
+{
+    bool isWaveEnd = false;
+};
+}  // namespace CubbyTower
+
+#endif  // CUBBYTOWER_END_WAVE_CHECKER_HPP
