25db1e5bea44b2d1b365f8e778fcddb5a371adcf
Chris Ohk <utilforever@gmail.com>
2022-06-18 15:24:10 +0900

    feat(combat): Delete function 'UpdateProjectileSystem()'

--- Sources/CubbyTower/Game.cpp
+++ Sources/CubbyTower/Game.cpp
@@ -31,7 +31,6 @@
 #include <CubbyTower/Systems/LineRenderSystem.hpp>
 #include <CubbyTower/Systems/PlaceSystem.hpp>
 #include <CubbyTower/Systems/PointRenderSystem.hpp>
-#include <CubbyTower/Systems/ProjectileSystem.hpp>
 #include <CubbyTower/Systems/ShapeRenderSystem.hpp>
 #include <CubbyTower/Systems/SizePulseAnimSystem.hpp>
 #include <CubbyTower/Systems/SpawnSystem.hpp>
@@ -135,7 +134,6 @@ void Simulate(entt::registry& registry, float deltaTime)
     UpdateTravelSystem(registry, deltaTime);
     UpdateSizePulseAnimSystem(registry, deltaTime);
     UpdateAttackSystem(registry, deltaTime);
-    UpdateProjectileSystem(registry, deltaTime);
     UpdateCollisionSystem(registry);
 }
 
