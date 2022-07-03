9d1f1fb4d6e6d0085926e584151fc35673cc86df
Chris Ohk <utilforever@gmail.com>
2022-07-03 19:41:26 +0900

    feat(combat): Add function 'UpdateDeathTimerSystem()'
    - Update death timers then kill finished entities

---
+++ Sources/CubbyTower/Systems/DeathTimerSystem.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#include <CubbyTower/Components/DeathTimer.hpp>
+#include <CubbyTower/Systems/DeathTimerSystem.hpp>
+
+namespace CubbyTower
+{
+void UpdateDeathTimerSystem(entt::registry& registry, float deltaTime)
+{
+    registry.view<DeathTimer>().each(
+        [&registry, deltaTime](auto entity, DeathTimer& deathTimer) {
+            const float delay = deathTimer.delay - deltaTime;
+
+            if (delay <= 0.0f)
+            {
+                registry.destroy(entity);
+                return;
+            }
+
+            deathTimer.delay = delay;
+        });
+}
+}  // namespace CubbyTower
