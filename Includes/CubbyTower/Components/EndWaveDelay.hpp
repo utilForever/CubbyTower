9935bfdbab06abd6b645af8d91f604f8a44b41ff
Chris Ohk <utilforever@gmail.com>
2022-08-06 20:00:55 +0900

    feat(combat): Add component 'EndWaveDelay'
    - This struct stores delay before ending the wave

---
+++ Includes/CubbyTower/Components/EndWaveDelay.hpp
@@ -0,0 +1,23 @@
+// Copyright (c) 2021 CubbyTower Team
+// Chris Ohk, Minkyu Lee, Minjune Yi
+// We are making my contributions/submissions to this project solely in our
+// personal capacity and are not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifndef CUBBYTOWER_END_WAVE_DELAY_HPP
+#define CUBBYTOWER_END_WAVE_DELAY_HPP
+
+namespace CubbyTower
+{
+//!
+//! \brief EndWaveDelay struct.
+//!
+//! This struct stores delay before ending the wave.
+//!
+struct EndWaveDelay
+{
+    float delay = 0.0f;
+};
+}  // namespace CubbyTower
+
+#endif  // CUBBYTOWER_END_WAVE_DELAY_HPP
