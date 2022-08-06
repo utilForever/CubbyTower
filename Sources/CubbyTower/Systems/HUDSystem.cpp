4d0adaf7b685ded14f824535dbaa0554b146e63c
Chris Ohk <utilforever@gmail.com>
2022-08-06 20:28:08 +0900

    feat(combat): Update code to render wave text

--- Sources/CubbyTower/Systems/HUDSystem.cpp
+++ Sources/CubbyTower/Systems/HUDSystem.cpp
@@ -8,6 +8,7 @@
 #include <CubbyTower/Components/Gold.hpp>
 #include <CubbyTower/Components/HUD.hpp>
 #include <CubbyTower/Components/TextRenderer.hpp>
+#include <CubbyTower/Components/WaveManagerInfo.hpp>
 #include <CubbyTower/Systems/HUDSystem.hpp>
 
 namespace CubbyTower
@@ -19,12 +20,26 @@ void UpdateHUDSystem(entt::registry& registry)
     {
         TextRenderer& textRenderer =
             registry.get<TextRenderer>(hud.currentWaveLabel);
-        textRenderer.text = "Wave ?";
+
+        if (registry.empty<Tag::WaveManager>())
+        {
+            textRenderer.text = "Finished";
+        }
+        else
+        {
+            auto waveManagerEntity = registry.view<Tag::WaveManager>()[0];
+            WaveManagerInfo& waveManager =
+                registry.get<WaveManagerInfo>(waveManagerEntity);
+
+            textRenderer.text =
+                "Wave " + std::to_string(waveManager.curWaveIndex + 1);
+        }
     }
 
     {
         Gold& gold = registry.get<Gold>(registry.view<Tag::Player>()[0]);
         TextRenderer& textRenderer = registry.get<TextRenderer>(hud.goldLabel);
+
         textRenderer.text = "Gold: $" + std::to_string(gold.amount);
     }
 }
